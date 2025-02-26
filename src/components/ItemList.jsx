21df086ca66c0cae5a1ff98ccf51c921d81118d9
MARCELO AUGUSTO ALVES FARIAS <marcelo.augsd@gmail.com>
2025-02-26 15:37:51 -0400

    versao 2

--- src/components/ItemList.jsx
+++ src/components/ItemList.jsx
@@ -9,6 +9,43 @@ const ItemList = ({ itemArray, items, idPath }) => {
       </div>
 
       <div className="item-list__container">
+        <table className="single-item__table">
+          <thead>
+            <tr>
+              <th className="single-item__th">
+                <div className="single-item__th__div">Number</div>
+              </th>
+              <th className="single-item__th">
+                <div className="single-item__th__div">Name</div>
+              </th>
+              <th className="single-item__th">
+                <div className="single-item__th__div">Type</div>
+              </th>
+              <th className="single-item__th">
+                <div className="single-item__th__div">Total</div>
+              </th>
+              <th className="single-item__th">
+                <div className="single-item__th__div">Hp</div>
+              </th>
+              <th className="single-item__th">
+                <div className="single-item__th__div">Attack</div>
+              </th>
+              <th className="single-item__th">
+                <div className="single-item__th__div">Defense</div>
+              </th>
+              <th className="single-item__th">
+                <div className="single-item__th__div">Sp.Atk</div>
+              </th>
+              <th className="single-item__th">
+                <div className="single-item__th__div">Sp.Def</div>
+              </th>
+              <th className="single-item__th">
+                <div className="single-item__th__div">Speed</div>
+              </th>
+            </tr>
+          </thead>
+        </table>
+
         {itemArray
           .filter((currentValue, index) => index < items)
           .map((currObj, index) => (
--- src/components/SingleItem.jsx
+++ src/components/SingleItem.jsx
@@ -1,7 +1,41 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-const singleItem = ({ id, name, sprites, idPath }) => {
+const singleItem = ({ id, name, sprites, types, stats, idPath }) => {
+  // Pegar todos os stats base do pokemon e colocar em um array statsBase
+  // fiz dessa forma porque nao tinha uma tag propria, vinha como String
+  let statsBase = [];
+
+  stats.forEach((currStats) => {
+    if (
+      [
+        "hp",
+        "attack",
+        "defense",
+        "special-attack",
+        "special-defense",
+        "speed",
+      ].includes(currStats.stat.name)
+    ) {
+      statsBase.push(currStats["base_stat"]);
+    }
+  });
+
+  // Soma todos os status base e fazer o total
+  let totalStats = statsBase.reduce((acc, curr) => acc + curr, 0);
+
+  // Pegar todos os types do pokemon e colocar em um Array
+  // Porq as vezes o Pokemon tem mais de um Type
+  let typesArray = [];
+
+  types.forEach((currTypes) => {
+    let typeName = currTypes.type.name;
+    let formattedName = typeName.charAt(0).toUpperCase() + typeName.slice(1);
+    typesArray.push(formattedName + " ");
+  });
+
+  const namePokemon = name.charAt(0).toUpperCase() + name.slice(1);
+
   return (
     <div>
       <Link to={`${idPath}/${id}`} className="single-item">
@@ -17,7 +51,15 @@ const singleItem = ({ id, name, sprites, idPath }) => {
           <div className="single-item__texts">
             <div className="single-item__2lines">
               <p className="single-item__title">{id}</p>
-              <p className="single-item__title">{name}</p>
+              <p className="single-item__title">{namePokemon}</p>
+              <p className="single-item__title">{typesArray}</p>
+              <p className="single-item__title">{totalStats}</p>
+              <p className="single-item__title">{statsBase[0]}</p>
+              <p className="single-item__title">{statsBase[1]}</p>
+              <p className="single-item__title">{statsBase[2]}</p>
+              <p className="single-item__title">{statsBase[3]}</p>
+              <p className="single-item__title">{statsBase[4]}</p>
+              <p className="single-item__title">{statsBase[5]}</p>
             </div>
           </div>
         </div>
